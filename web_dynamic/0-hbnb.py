c8109e7ed0e03c7c1aa2611a6df49af9feff3fbe
zack6yuan <zack6yuan@gmail.com>
2024-11-28 21:36:48 +0000

    update render_template variable.

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -37,7 +37,8 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places
+                           cache_id = uuid.uuid4())
 
 
 if __name__ == "__main__":
