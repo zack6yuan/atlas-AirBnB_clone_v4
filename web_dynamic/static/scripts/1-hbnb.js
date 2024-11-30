f969292e838e4c5a4a80f8758833676ed3c5b423
clay <rpctropic13@gmaiail.com>
2024-11-30 08:15:15 -0600

    amenity check

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* global $ */
+
+
+$(document).ready(function () {
+  let checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/* global $ */
+
+$(document).ready(function () {
+  const selectedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if (this.checked) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+
+    const amenitiesList = Object.values(selectedAmenities).join(', ');
+    if (amenitiesList.length > 0) {
+      $('div.amenities > h4').text(amenitiesList);
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+
+  // Check API status
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
