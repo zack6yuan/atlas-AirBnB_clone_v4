2eba68e0c9ddadf75ab10e39537a8021bb02be31
clay <rpctropic13@gmaiail.com>
2024-11-30 05:00:26 -0600

    added cache_id

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,6 +7,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -18,7 +20,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -33,12 +35,13 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    cache_id = uuid.uuid4()
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                     states=st_ct,
                     amenities=amenities,
-                    places=places
-                    cache_id = uuid.uuid4())
+                    places=places,
+                    cache_id = cache_id)
 
 
 if __name__ == "__main__":
