b6438a70abc1e5bf8ae316cdba914e70b4dad5ef
zack6yuan <zack6yuan@gmail.com>
2024-12-03 05:33:00 +0000

    update JavaScript comments.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* global $ */
 
+// Execute when DOM is loaded
 $(document).ready(function () {
   const selectedAmenities = {};
 
@@ -22,11 +23,13 @@ $(document).ready(function () {
     }
   });
 
-  // Check API status
+  // Check API status, make request
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
+      // Add class 'available'
       $('#api_status').addClass('available');
     } else {
+      // Remove class 'available'
       $('#api_status').removeClass('available');
     }
   });
