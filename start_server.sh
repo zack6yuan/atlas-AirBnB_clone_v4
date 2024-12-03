3d0c364b8f515d21328469205df48e4ee4d764d0
clay <rpctropic13@gmaiail.com>
2024-12-03 17:01:05 -0600

    server start up

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db python3 -m web_dynamic.4-hbnb
+
+python3 -m api.v1.app
 # Change "python3.8" to whatever version of python works best for you, perhaps just "python3"
 # Change "4-hbnb" (at the very end) to whichever hbnb you are testing. 4-hbnb is the last one.
 # This script is designed to work with Bash on Linux
