3ee6123a7e0e36339e9fc418e4c0f40755fde8cd
zack6yuan <zack6yuan@gmail.com>
2024-12-03 06:58:29 +0000

    trying.

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -30,42 +30,41 @@ $(document).ready(function () {
       $("#api_status").removeClass("available");
     }
   });
-
   // AJAX request
-  $.ajax({
-    url: "http://0.0.0.0:5001/api/v1/places_search/",
-    type: "POST",
-    contentType: "application/json",
-    success: function (result) {
-      for (let x = 0; x < result.length; x++) {
-        place = result[x];
-        const data = (`
-          <article>
-            <div class="title_box">
-              <h2>${place.name}</h2>
-              <div class="price_by_night">
-                <h2>${place.price_by_night}</h2>
-                <div class="information">
-                  <h2>${place.max_guest}</h2>
-                  <h2>${place.number_rooms}</h2>
-                  <h2>${place.number_bathrooms}</h2>
-                  <div class="user"></h2>
-                    <h2>${place.user.first_name} ${place.user.last_name}</h2>
-                    <div class="description>
-                      <h2>${place.description}</h2>
+  $('button').click(function() {
+    $.ajax({
+      url: "http://0.0.0.0:5001/api/v1/places_search/",
+      type: "POST",
+      contentType: "application/json",
+      success: function (result) {
+        for (let x = 0; x < result.length; x++) {
+          place = result[x];
+          const data = (`
+            <article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">
+                  <h2>${place.price_by_night}</h2>
+                  <div class="information">
+                    <h2>${place.max_guest}</h2>
+                    <h2>${place.number_rooms}</h2>
+                    <h2>${place.number_bathrooms}</h2>
+                    <div class="user"></h2>
+                      <h2>${place.user.first_name} ${place.user.last_name}</h2>
+                      <div class="description>
+                        <h2>${place.description}</h2>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </article>
-          }
-        `);
-        ('section.place').append(data);
-        console.log(result);
+            </article>
+            }
+          `);
+          ('section.place').append(data);
+          console.log(result);
+        }
       }
-    }
+    })
   })
-
-
 });
