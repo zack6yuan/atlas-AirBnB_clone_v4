323ac809396b5a87005b1463a48b029d2f84554f
zack6yuan <zack6yuan@gmail.com>
2024-12-03 05:28:14 +0000

    update js comments.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 /* global $ */
 
-
+// Execute when DOM is loaded
 $(document).ready(function () {
   let checkedAmenities = {};
+  // Listen for changes
   $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
+      // Store Amenity ID in variable 
       checkedAmenities[$(this).data('id')] = $(this).data('name');
     } else {
+      // Remove Amentiy ID from variable
       delete checkedAmenities[$(this).data('id')];
     }
+    // List of checked Amenities
     let lst = Object.values(checkedAmenities);
     if (lst.length > 0) {
       $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
